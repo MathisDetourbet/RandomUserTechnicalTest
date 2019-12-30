acd8765a05e29548974b4d9d1d29b6aa01f082a9
Mathis Detourbet <mathis.detourbet@gmail.com>
2019-12-30 10:06:07 +0100

    Initial Commit

---
+++ RandomUser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomUser
+//
+//  Created by Mathis Detourbet on 30/12/19.
+//  Copyright © 2019 Mathis Detourbet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
